91c86494b6fba99970ede0fcc04a159863434090
Sagar Mitra <mitrasagar10@gmail.com>
2026-01-13 19:03:45 +0530

    feat: initialize devtinder-frontend project with Vite, React, Tailwind CSS and Shadcn UI

---
+++ README.md
@@ -0,0 +1,5 @@
+## DevTinder is a Tinder clone for developers
+
+DevTinder is a Tinder clone for developers. It is a social network for developers to connect with other developers.
+
+It is a work in progress.
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="text-5xl text-red-500">
+      App
+      <Button>Button</Button>
+    </div>
+  );
+};
+
+export default App;
