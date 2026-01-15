6d804653c15ddc52e0e95d0610a912a3ecd75dec
Sagar Mitra <mitrasagar10@gmail.com>
2026-01-15 11:02:48 +0530

    feat: set up redux store and create user slice

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import Navbar from "./components/Navigation/Navbar";
 import Footer from "./components/Navigation/Footer";
+import appStore from "./store/appStore";
+import { Toaster } from "sonner";
 
 const App = () => {
   return (
-    <div className="max-md:px-4 ">
-      <Navbar />
-      <Outlet />
-      <Footer />
-    </div>
+    <Provider store={appStore}>
+      <div className="max-md:px-4 z-100 ">
+        <Navbar />
+        <Outlet />
+        <Footer />
+        <Toaster position="top-right" />
+      </div>
+    </Provider>
   );
 };
 
